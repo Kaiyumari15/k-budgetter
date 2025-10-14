0bca5c5155b93931d868d8e35010a93487419329
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-10-14 13:28:09 +0100

    Define account type in model/accounts.ts

---
+++ model/accounts.ts
@@ -0,0 +1,9 @@
+type account = {
+    name: string;
+    type: string;
+    initialBalance: number;
+    currentBalance: number;
+    description?: string;
+    createdAt: string; // ISO 8601 date string
+    updatedAt: string; // ISO 8601 date string
+};
