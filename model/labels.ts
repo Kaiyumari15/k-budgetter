07ee628236b6dae986e4191465c96d44b7f3c491
Callum Taylor <150924494+Kaiyumari15@users.noreply.github.com>
2025-10-14 19:28:09 +0100

    Define `label` type

---
+++ model/labels.ts
@@ -0,0 +1,8 @@
+type labels = {
+    id: string; // SurrealDB record ID
+    name: string;
+    description: string;
+    colour: string; // Hex colour code, e.g., "#FF5733"
+    createdAt: string; // ISO 8601 timestamp
+    updatedAt: string; // ISO 8601 timestamp
+}
